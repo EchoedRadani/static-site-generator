809e08a8b171bc9764b7a5b7db825687badd5a41
EchoedRadani <david_hershberger@outlook.com>
2026-02-10 12:09:53 -0500

    added copy_static_to_public

--- src/main.py
+++ src/main.py
@@ -1,6 +1,24 @@
 from textnode import TextNode, TextType
+import os
+import shutil
+
+
+def copy_static_to_public(source, destination):
+    if not os.path.exists(destination):
+        os.mkdir(destination)
+    for item in os.listdir(source):
+        source_path = os.path.join(source, item)
+        destination_path = os.path.join(destination, item)
+        print(f" * {source_path} -> {destination_path}")
+        if os.path.isdir(source_path):  
+            copy_static_to_public(source_path, destination_path)
+        else:
+            shutil.copy2(source_path, destination_path)
+
 
 def main():
-    print(TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev"))
+    source = "./static"
+    destination = "./public"
+    copy_static_to_public(source, destination)
 
 main()
